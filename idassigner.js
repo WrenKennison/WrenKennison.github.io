024f060d9eb20ef1b7698d4fee21c2ab0a71c867
WrenKennison <wrenkenson@gmail.com>
2025-06-13 05:03:24 -0400

    Added fun stuff ! ! !

---
+++ idassigner.js
@@ -0,0 +1,26 @@
+splash = new Array();
+
+splash[0] = "Wren Venison";
+splash[1] = "Freya Kowalski";
+splash[2] = "Phoebe Wonton";
+splash[3] = "Jackson Sniffles";
+splash[4] = "Remy Bobbin";
+splash[5] = "Oskar Scholastic";
+splash[6] = "Dr. Gregory House";
+splash[7] = "Donivan Rutle";
+splash[8] = "Angus Jarring";
+splash[9] = "Pablo Pebble";
+splash[10] = "Zane Muller";
+splash[11] = "Dido Wonton";
+splash[12] = "Boris Wonton";
+splash[13] = "Adil Bowler";
+splash[14] = "Lauren Boswell";
+splash[15] = "Kate Lonnole";
+splash[16] = "Jannifer Rochester";
+splash[17] = "Zaid Adderall";
+splash[18] = "Bella Othercrab";
+
+RandomNumber = Math.floor(Math.random() * splash.length);
+//Math.floor(Math.random() * splash.length)
+
+document.write(splash[RandomNumber]);
